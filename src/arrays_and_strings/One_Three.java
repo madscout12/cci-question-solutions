9b8b55d1eb35507ceeae9e7a32cdbbcf17af2358
madscout <zkoopmans@gmail.com>
2017-10-01 23:09:24 -0500

    Finished Arrays Strings Question 1.3.

---
+++ src/arrays_and_strings/One_Three.java
@@ -0,0 +1,54 @@
+package arrays_and_strings;
+
+import java.nio.charset.CharsetEncoder;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class One_Three {
+    private HashMap<Character, ArrayList<Character>> theMap;
+    private String one;
+
+    public One_Three(String orig) {
+        one = orig;
+        theMap = new HashMap<>();
+        makeHashMap();
+    }
+
+    public boolean isPermutation(String two) {
+
+        if (one == null || two == null) return false;
+
+        ArrayList<Character> list;
+
+        for (int i = 0; i < two.length(); i++) {
+            list = theMap.get(two.charAt(i));
+
+            if (list == null || list.isEmpty()) {
+                return false;
+            } else {
+                list.remove(0);
+                if (list.isEmpty()) theMap.remove(two.charAt(i));
+            }
+        }
+        return theMap.isEmpty();
+
+    }
+
+    private void makeHashMap() {
+        if (one == null) return;
+
+        ArrayList<Character> list;
+        for (int i = 0; i < one.length(); i++) {
+            list = theMap.get(one.charAt(i));
+
+            if (list == null) {
+                list = new ArrayList<>();
+                list.add(one.charAt(i));
+                theMap.put(one.charAt(i), list);
+            } else {
+                list.add(one.charAt(i));
+            }
+
+        }
+    }
+}
---
+++ test/Arrays_Strings/One_Three_Test.java
@@ -0,0 +1,51 @@
+package Arrays_Strings;
+
+import arrays_and_strings.One_Three;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class One_Three_Test {
+
+    @Test
+    public void checkNull() {
+        String notNull = "Not Null";
+
+        One_Three one = new One_Three(null);
+        Assert.assertFalse(one.isPermutation(notNull));
+
+        one = new One_Three(notNull);
+        Assert.assertFalse(one.isPermutation(null));
+
+        one = new One_Three(null);
+        Assert.assertFalse(one.isPermutation(null));
+    }
+
+    @Test
+    public void isAPermutation() {
+        String orig = "permutation";
+        String perm = "prmttneuaio";
+
+        One_Three one = new One_Three(orig);
+        Assert.assertTrue(one.isPermutation(perm));
+
+    }
+
+    @Test
+    public void isNotAPermutation() {
+        String orig = "permutation";
+        String perm = "prmttneuai";
+
+        One_Three one = new One_Three(orig);
+        Assert.assertFalse(one.isPermutation(perm));
+
+        perm = "pprmttneuai";
+
+        one = new One_Three(orig);
+        Assert.assertFalse(one.isPermutation(perm));
+
+    }
+
+}
+
+
+
