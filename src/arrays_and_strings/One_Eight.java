9517e5e07129466fffba790b051621984f7c2a05
madscout <zkoopmans@gmail.com>
2017-10-07 13:33:27 -0500

    Adding definition of problem in comments

--- src/arrays_and_strings/One_Eight.java
+++ src/arrays_and_strings/One_Eight.java
@@ -1,3 +1,10 @@
+/*
+* 1.8 Assume you have a method "isSubstring" (for us, java provides the String class's "contains" method)
+*  which checks if one word is a substring of another. Given two strings, s1, and s2, write code to check
+*   if s2 is a rotation of s1 usign only one call to isSubstring (e.g. "waterbottles" is a rotation of
+*   "erbottlewart".
+*/
+
 package arrays_and_strings;
 
 public class One_Eight {
